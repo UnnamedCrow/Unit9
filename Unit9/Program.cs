8ebebf3d4801c4b81343a05360a0e4fa53f6ab07
Максим Самисько <masechk@gmail.com>
2023-04-05 23:15:24 +0300

    Unit9 - Homework: Added arrray with 5 types of exceptions, make try-catch block

--- Unit9/Program.cs
+++ Unit9/Program.cs
@@ -4,12 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Unit9
 {
+    //    Создайте свой тип исключения.
+    //    Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. 
+    //    Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+    //    В блоке catch выведите в консольном сообщении текст исключения.
+    class UserException : Exception
+    {
+        public int Value { get; set; }
+        public UserException(string message, int Value) : base(message) 
+        {
+            this.Value = Value;
+        }
+   
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Array with 5 types of Exceptions
+            Exception[] ExceptionArray = new Exception[5];
+            ExceptionArray[0] = new UserException("UserException", 1);
+            ExceptionArray[1] = new FormatException();
+            ExceptionArray[2] = new ArgumentException();
+            ExceptionArray[3] = new IndexOutOfRangeException();
+            ExceptionArray[4] = new DivideByZeroException();
+            Console.WriteLine("Enter number from 1 to 10");
+            try
+            {
+                int Number_A;
+                if (!int.TryParse(Console.ReadLine(), out Number_A))
+                    throw new FormatException();
+                if (Number_A < 1 || Number_A > 10)
+                    throw new UserException("Number must be from 1 to 10", Number_A);
+                int Number_B;
+                Number_B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Number_A / Number_B);
+            }
+            catch(UserException ex)
+            { 
+                Console.WriteLine(ex.Message, ex.Value);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Wront format");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Number_B = 0");
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Wrong argument");
+            }
+            Console.ReadLine();
         }
     }
 }
